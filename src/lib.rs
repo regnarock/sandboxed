07dc7cb2356a66eadfa6d5dd2dcc6a57117bec5e
Thomas Wilgenbus <thomas.wilgenbus@happn.fr>
2023-10-03 15:41:44 +0200

    Expand default plugins and extract game state

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,10 @@
 
 mod actions;
 mod audio;
+mod engine;
 mod loading;
 mod menu;
 mod player;
-mod engine;
 
 use crate::actions::ActionsPlugin;
 use crate::audio::InternalAudioPlugin;
@@ -13,17 +13,19 @@ use crate::loading::LoadingPlugin;
 use crate::menu::MenuPlugin;
 use crate::player::PlayerPlugin;
 
-use bevy::app::App;
 #[cfg(debug_assertions)]
 use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
+use bevy::log::LogPlugin;
 use bevy::prelude::*;
+use bevy::render::extract_resource::ExtractResourcePlugin;
+use bevy::{app::App, render::extract_resource::ExtractResource};
 use engine::EnginePlugin;
 
 // This example game uses States to separate logic
 // See https://bevy-cheatbook.github.io/programming/states.html
 // Or https://github.com/bevyengine/bevy/blob/main/examples/ecs/state.rs
 #[derive(States, Default, Clone, Eq, PartialEq, Debug, Hash)]
-enum GameState {
+pub enum GameState {
     // During the loading State the LoadingPlugin will load our assets
     #[default]
     Loading,
@@ -33,22 +35,65 @@ enum GameState {
     Menu,
 }
 
+#[derive(Resource, Default, Clone, ExtractResource)]
+pub struct GameStateRes {
+    state: GameState,
+}
+impl GameStateRes {
+    fn new(state: GameState) -> GameStateRes {
+        GameStateRes { state }
+    }
+}
+
 pub struct GamePlugin;
 
 impl Plugin for GamePlugin {
     fn build(&self, app: &mut App) {
-        app.add_state::<GameState>().add_plugins((
-            LoadingPlugin,
-            MenuPlugin,
-            ActionsPlugin,
-            InternalAudioPlugin,
-            PlayerPlugin,
-            EnginePlugin,
-        ));
+        app.add_state::<GameState>()
+            .add_plugins((
+                LoadingPlugin,
+                MenuPlugin,
+                ActionsPlugin,
+                InternalAudioPlugin,
+                PlayerPlugin,
+                EnginePlugin,
+                ExtractResourcePlugin::<GameStateRes>::default(),
+            ))
+            .init_resource::<GameStateRes>()
+            .add_systems(PostUpdate, extract_game_state);
+
+        #[cfg(not(debug_assertions))]
+        app.add_plugins(LogPlugin {
+            level: bevy::log::Level::INFO,
+            filter: "info,wgpu_core=warn,wgpu_hal=warn".into(),
+        });
 
         #[cfg(debug_assertions)]
         {
+            app.add_plugins(LogPlugin {
+                level: bevy::log::Level::DEBUG,
+                filter: "debug,wgpu_core=warn,wgpu_hal=warn,sandboxed=debug".into(),
+            });
             app.add_plugins((FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin::default()));
         }
+    }
+}
+
+/// Manual extract of [`GameState`] to [`GameStateRes`] (for later exactraction with [`ExtractResourcePlugin`]) since [`State`] cannot be extracted.
+///
+/// To be ran during [`PostUpdate`] to make sure [`GameStateRes`] is updated in renderer as soon as possible
+///
+pub fn extract_game_state(
+    mut commands: Commands,
+    new_state: Res<State<GameState>>,
+    current_state_res: Option<ResMut<GameStateRes>>,
+) {
+    if new_state.is_changed() {
+        if let Some(mut current_state) = current_state_res {
+            current_state.state = new_state.get().to_owned();
+        }
+    } else {
+        let current_state_res = GameStateRes::new(new_state.get().to_owned());
+        commands.insert_resource(current_state_res);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
 // disable console on windows for release builds
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use bevy::prelude::*;
-use bevy::window::PrimaryWindow;
-use bevy::winit::WinitWindows;
-use bevy::DefaultPlugins;
+use bevy::{
+    a11y::AccessibilityPlugin,
+    core_pipeline::CorePipelinePlugin,
+    diagnostic::DiagnosticsPlugin,
+    input::InputPlugin,
+    prelude::*,
+    render::{pipelined_rendering::PipelinedRenderingPlugin, RenderPlugin},
+    scene::ScenePlugin,
+    sprite::SpritePlugin,
+    text::TextPlugin,
+    time::TimePlugin,
+    ui::UiPlugin,
+    window::PrimaryWindow,
+    winit::{WinitPlugin, WinitWindows},
+};
 use sandboxed::GamePlugin;
 use std::io::Cursor;
 use winit::window::Icon;
@@ -13,18 +24,42 @@ fn main() {
     App::new()
         .insert_resource(Msaa::Off)
         .insert_resource(ClearColor(Color::rgb(0.4, 0.4, 0.4)))
-        .add_plugins(DefaultPlugins.set(WindowPlugin {
-            primary_window: Some(Window {
-                title: "Sandboxed".to_string(),
-                resolution: (800., 600.).into(),
-                // Bind to canvas included in `index.html`
-                canvas: Some("#bevy".to_owned()),
-                // Tells wasm not to override default event handling, like F5 and Ctrl+R
-                prevent_default_event_handling: false,
+        .add_plugins((
+            (
+                TaskPoolPlugin::default(),
+                TypeRegistrationPlugin::default(),
+                FrameCountPlugin::default(),
+                TimePlugin::default(),
+                TransformPlugin::default(),
+                HierarchyPlugin::default(),
+                DiagnosticsPlugin::default(),
+                AssetPlugin::default(),
+            ),
+            InputPlugin::default(),
+            AccessibilityPlugin,
+            RenderPlugin::default(),
+            ImagePlugin::default(),
+            PipelinedRenderingPlugin::default(),
+            WinitPlugin::default(),
+            ScenePlugin::default(),
+            CorePipelinePlugin::default(),
+            SpritePlugin::default(),
+            TextPlugin::default(),
+            UiPlugin::default(),
+            WindowPlugin {
+                primary_window: Some(Window {
+                    title: "Sandboxed".to_string(),
+                    resolution: (800., 600.).into(),
+                    // Bind to canvas included in `index.html`
+                    canvas: Some("#bevy".to_owned()),
+                    // Tells wasm not to override default event handling, like F5 and Ctrl+R
+                    prevent_default_event_handling: false,
+                    //                        present_mode: PresentMode::AutoNoVsync, // TODO allow for a timed system within the shaders
+                    ..default()
+                }),
                 ..default()
-            }),
-            ..default()
-        }))
+            },
+        ))
         .add_plugins(GamePlugin)
         .add_systems(Startup, set_window_icon)
         .run();
